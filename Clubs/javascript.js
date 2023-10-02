73f42793601ee08094757e31fa3decb5a449d901
parthiv_ghose <parthivghose2919@gmail.com>
2023-10-03 03:22:05 +0530

    Added a Simple Webpage for your organisation and making it more responsive using Javascript

---
+++ Clubs/javascript.js
@@ -0,0 +1,6 @@
+const joinButton = document.querySelector('.button');
+function handleJoinButtonClick() {
+    
+    alert('Thank you for your interest in joining IEEE Club!');
+}
+joinButton.addEventListener('click', handleJoinButtonClick);
